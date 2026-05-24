a8ecd7ecfa59d7e641920c7a2a36e266f5ffa280
Lilyennd <victoriariffo21@gmail.com>
2026-05-24 13:36:33 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/bitacorasVuelos/BitacorasVuelosApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.bitacorasVuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacorasVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacorasVuelosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/GestionDrones/v1/bitacorasVuelos/BitacorasVuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.bitacorasVuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacorasVuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
