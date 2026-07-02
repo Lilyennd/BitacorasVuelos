343b62120f137c4ec674d92572b188c91ef9adfb
Lilyennd <victoriariffo21@gmail.com>
2026-07-02 15:40:39 -0400

    webconfig v1.2

--- src/main/java/cl/GestionDrones/v1/bitacorasVuelos/webconfig/WebConfig.java
+++ src/main/java/cl/GestionDrones/v1/bitacorasVuelos/webconfig/WebConfig.java
@@ -1,24 +1,31 @@
 package cl.GestionDrones.v1.bitacorasVuelos.webconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebConfig {
+    
+    @Value("${api.url.planesdevuelos}")
+    private String planesDeVuelosUrl;
+
+    @Value("${api.url.incidencias}")
+    private String incidenciasUrl;
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
 
-
     @Bean
     public WebClient planesDeVuelosWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8083/api/v1/planesDeVuelos").build();
+        return builder.baseUrl(planesDeVuelosUrl).build();
     }
 
     @Bean
     public WebClient apiIncidenciasWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8089/api/v1/incidencias").build();
+        return builder.baseUrl(incidenciasUrl).build();
     }
 }
